109bd626541af08b343abae4d8fe687a180c8c2a
Yutaka HARA <yutaka.hara+github@gmail.com>
2011-02-19 18:15:08 +0900

    introduced slim

--- lib/spellbook/server.rb
+++ lib/spellbook/server.rb
@@ -1,10 +1,26 @@
 require 'spellbook'
 require 'sinatra/base'
+require 'slim'
+
+module Sinatra
+  module Templates
+    unless defined? slim
+      def slim(template, options={}, locals={})
+        render :slim, template, options, locals
+      end
+    end
+  end
+end
 
 module SpellBook
   class Server < Sinatra::Base
     get '/' do
-      "hi"
+      slim <<-EOD
+h1 SpellBook
+
+p
+  | hi
+      EOD
     end
   end
 end
