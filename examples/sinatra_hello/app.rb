76a33e28b5890e4546b7c32232f10a22e6174223
Yutaka HARA <yutaka.hara+github@gmail.com>
2011-04-22 18:27:03 +0900

    Slop'ified sample sinatra app

--- examples/sinatra_hello/app.rb
+++ examples/sinatra_hello/app.rb
@@ -1,19 +1,15 @@
-require 'optparse'
+require 'slop'
 
-port = prefix = nil
-OptionParser.new{|o|
-  o.on("--port PORT"){|n|
-    port = n
-  }
-  o.on("--prefix PATH"){|s|
-    prefix = s
-  }
-}.parse!(ARGV)
+opts = Slop.parse!(:help => true) do
+  on 'p', 'port', true,        :default => 8080, :as => :integer
+  on 'e', 'environment', true, :default => "production"
+  on      'prefix', true,      :default => ""
+end
 
 require 'sinatra'
 
-set :port, (port || 12345)
+set :port, opts[:port]
 
-get "#{prefix}/" do
-  "hi"
+get "#{opts[:prefix]}/" do
+  "hi! this is a sample Spellbook application."
 end
